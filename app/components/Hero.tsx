9a54fa70e582368755708615e67c4313a36e4d9d
seyitbugraerden <seyitbugraerden@gmail.com>
2025-02-23 23:22:08 +0300

    Hero Section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <section className="bg-[#040F48] text-white py-24 curved-edge text-center">
+      <div className="flex flex-col justify-center items-center gap-4">
+        <h1 className="text-4xl font-bold">XE Currency Converter</h1>
+        <p className="font-thin">Check live foreign currency exchange rates</p>
+      </div>
+    </section>
+  );
+};
